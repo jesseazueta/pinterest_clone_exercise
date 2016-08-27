578b31fe83eac7cb1339e1e157205faca59e4b2f
Jesus Barrenechea <jesseazueta@gmail.com>
2016-08-27 15:37:27 -0400

    Add and configure acts_as_votable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pins
+  devise_for :users
+resources :pins do
+  member do
+    put "like",    to: "pins#upvote"
+  end
+end
 
-  root 'pins#index'
+root "pins#index"
 end
